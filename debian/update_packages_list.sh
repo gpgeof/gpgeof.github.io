f88dc5ce4c68952ce47e3f708fd6a44bc1013b1e
Dirack <rodolfo_profissional@hotmail.com>
2025-02-15 12:19:30 -0300

    Deploy shellunity 0.3.0

--- debian/update_packages_list.sh
+++ debian/update_packages_list.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Verificar a última atualização
 LAST_RELEASE=$(curl https://api.github.com/repos/Dirack/ShellUnity/releases/latest -s | sed -n '/tag_name/p' | cut -d":" -f2 | cut -d'"' -f2)
 
@@ -15,9 +14,6 @@ if ls *.deb.zip
 then
     rm *.deb.zip
 fi
-
-# Baixar atualização da shellunity
-#https://github.com/Dirack/ShellUnity/releases/latest
 
 # This is a script to update the Packages list with the following command
 dpkg-scanpackages -m . /dev/null > Packages
