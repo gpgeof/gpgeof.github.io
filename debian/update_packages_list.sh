4b990de7f13f6c737b6506d4ddd95a35887ed03f
Dirack <rodolfo_profissional@hotmail.com>
2025-02-22 18:48:46 -0300

    Refactor: Update messages

--- debian/update_packages_list.sh
+++ debian/update_packages_list.sh
@@ -4,6 +4,8 @@ ORGANIZATIONS=("Dirack" "Dirack")
 REPOS=("ShellUnity" "Shellinclude")
 PACKAGENAMES=("shellunity" "shellinclude")
 
+echo "Check repositories for updates"
+
 for i in $(seq 0 1 1)
 do
 
@@ -11,6 +13,8 @@ do
 
     # Verificar a última atualização
     LAST_RELEASE=$(curl https://api.github.com/repos/${ORGANIZATIONS[$i]}/${REPOS[$i]}/releases/latest -s | sed -n '/tag_name/p' | cut -d":" -f2 | cut -d'"' -f2)
+
+    echo "Latest Release Found: ${LAST_RELEASE}"
 
     # Tem esta última release?
     if [ ! -f "${PACKAGENAMES[$i]}_${LAST_RELEASE##*v}_all.deb" ]
@@ -23,7 +27,8 @@ do
     then
         rm *.deb.zip
     fi
-
-    # This is a script to update the Packages list with the following command
-    dpkg-scanpackages -m . /dev/null > Packages
 done
+
+echo "Scan packages with dpkg"
+# This is a script to update the Packages list with the following command
+dpkg-scanpackages -m . /dev/null > Packages
